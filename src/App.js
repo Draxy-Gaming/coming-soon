0211c880c5a2efee60274391254790dc7d89f79f
Draxy-Gaming <140737843+Draxy-Gaming@users.noreply.github.com>
2023-12-15 17:47:50 +0000

    few

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
+
+import '@leenguyen/react-flip-clock-countdown/dist/index.css';
+
 
 const App = () => {
   return <div>react app</div>;
