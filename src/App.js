d2fda12234d92830a0e2c1b91612669523672ecd
Draxy-Gaming <140737843+Draxy-Gaming@users.noreply.github.com>
2023-12-15 18:00:40 +0000

    jf

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
 
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 
-
+import VideoBg from '../src/assets/video.mp4';
 const App = () => {
-  return <div>react app</div>;
+
+  return <section className='page'>
+
+    <div className='overlay'></div>
+    <video src={VideoBg}></video>
+      </section>;
 };
 
 export default App;
