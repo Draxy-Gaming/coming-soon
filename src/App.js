ea6f436c14e92998291fecac0aedb12d639927f0
Draxy-Gaming <140737843+Draxy-Gaming@users.noreply.github.com>
2023-12-15 18:23:45 +0000

    erterg

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
 
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
+import './styles.css';
 
 import VideoBg from '../src/assets/video.mp4';
 const App = () => {
@@ -9,7 +10,13 @@ const App = () => {
   return <section className='page'>
 
     <div className='overlay'></div>
-    <video src={VideoBg}></video>
+    <video src={VideoBg} autoPlay loop muted></video>
+     
+    <div className='page__content'>
+      <h1>Launching Soon</h1>
+      <h3>Leave your email and we will let you know once we are live.</h3>
+    </div>
+
       </section>;
 };
 
